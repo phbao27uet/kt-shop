da5524c37563e17f91c57be6c75e8c72fd2b1b1d
phbao272 <pqbao27@gmail.com>
2022-04-11 20:18:44 +0700

    update slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -31,7 +31,7 @@ const Slider = () => {
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 5000,
+                    delay: 2500,
                     disableOnInteraction: false,
                 }}
                 pagination={{

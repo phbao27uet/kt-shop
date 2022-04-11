7c900a724e01943cc3acf7d8229175f70406dc25
phbao272 <pqbao27@gmail.com>
2022-04-11 19:44:26 +0700

    update loading

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { getKeyboard } from '../redux/keyboard/keyboardSlice'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { LoadingOutlined } from '@ant-design/icons'
+import { Spin } from 'antd'
 
 import CreateRoutes from '../router/CreateRoutes'
 import Header from './Header'
@@ -10,21 +13,44 @@ import Footer from './Footer'
 import 'antd/dist/antd.css'
 
 const Layout = () => {
+    const [isLoading, setLoading] = useState(true)
+
+    const loading = useSelector((state) => state.keyboard.loading)
+
     const dispatch = useDispatch()
 
     useEffect(async () => {
         dispatch(getKeyboard())
     }, [])
 
+    useEffect(() => {
+        if (!loading) {
+            setTimeout(() => setLoading(loading), 1500)
+        }
+    }, [loading])
+
+    const antIcon = <LoadingOutlined style={{ fontSize: 40 }} spin />
+
     return (
         <div>
-            <Header />
-            <div className="container">
-                <div className="main">
-                    <CreateRoutes />
+            {isLoading ? (
+                <div className="loading">
+                    <div>
+                        <Spin indicator={antIcon} />
+                    </div>
+                    <div className="loading__txt">Đợi mình 1 xíu nhé sắp xong rùi!!!</div>
                 </div>
-            </div>
-            <Footer />
+            ) : (
+                <>
+                    <Header />
+                    <div className="container">
+                        <div className="main">
+                            <CreateRoutes />
+                        </div>
+                    </div>
+                    <Footer />
+                </>
+            )}
         </div>
     )
 }
